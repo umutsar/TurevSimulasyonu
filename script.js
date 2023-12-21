d4f9f660b58c59f16829670c5b336e414ac8adcb
Umut Şar <contact@umutsar.net>
2023-12-21 23:36:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // CODED BY UMUTSAR (FOR CALCULUS PROJECT) - github.com/umutsar (MIT LICENCE)
 // CONTACT@UMUTSAR.COM
-// .MATH111 CALCULUS PROJECT, Lecturer: Assoc. Prof. Dr. Ece Yetkin Celikel
-
+// MATH111 CALCULUS PROJECT, Lecturer: Assoc. Prof. Dr. Ece Yetkin Celikel
 
 const vector = document.getElementById('vector');
 const vector3 = document.getElementById('vector3');
@@ -19,7 +18,6 @@ const taban = document.getElementById('taban');
 
 let kök, kök1, kök2, tureveGoreAlan, tabanValue;
 
-
 // Fonksiyonlar
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -28,18 +26,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 function sin(dereceSin) {
-    let radyanSin = (parseInt(dereceSin) * Math.PI) / 180;
+    let radyanSin = (Number(dereceSin) * Math.PI) / 180;
     return Math.sin(radyanSin);
 }
 
 function cos(dereceCos) {
-    let radyanCos = (parseInt(dereceCos) * Math.PI) / 180;
+    let radyanCos = (Number(dereceCos) * Math.PI) / 180;
     return Math.cos(radyanCos);
 }
 
 // Taban uzunluğu ve kenarların birinin uzunluğu ile maximum kesit alanı bulma fonkisyonu.
 function turevHesapla(kenar) {
-    tabanValue = parseInt(vector2yazi.innerText);
+    tabanValue = Number(vector2yazi.innerText);
     kök1 = -(tabanValue + Math.sqrt((tabanValue ** 2) + 8 * (kenar ** 2))) / 4
     kök2 = -(tabanValue - Math.sqrt((tabanValue ** 2) + 8 * (kenar ** 2))) / 4
     if (kök1 <= kök2) {
@@ -49,7 +47,8 @@ function turevHesapla(kenar) {
         kök = kök1;
     }
 
-    tureveGoreAlan = parseInt((tabanValue + kök) * (Math.sqrt(kenar ** 2 - kök ** 2)));
+    tureveGoreAlan = Number((tabanValue + kök) * (Math.sqrt(kenar ** 2 - kök ** 2)));
+    console.log(tureveGoreAlan)
 }
 
 
@@ -96,49 +95,58 @@ let anlikSuYuksekligi;
 function updateAngle(angle) {
     let aci = document.getElementById("aci1");
     if (angle) {
-        let parsedAngle = parseInt(angle);
+        let parsedAngle = Number(angle);
+        let parsedAngleFixed = Number(angle).toFixed(1);
 
         if (parsedAngle < 0) {
-            aci.innerHTML = `${-parsedAngle}<span class="dereceIsareti">°</span>`;
-            aci2.innerHTML = `${180 + parsedAngle}<span class="dereceIsareti">°</span>`;
+            aci.innerHTML = `${-Number(parsedAngleFixed)}<span class="dereceIsareti">°</span>`;
+            aci2.innerHTML = `${180 + Number(parsedAngleFixed)}<span class="dereceIsareti">°</span>`;
 
-            vector.innerHTML = `${-parsedAngle}<span class="dereceIsareti">°</span>`;
-            vector3.innerHTML = `${180 + parsedAngle}<span class="dereceIsareti">°</span>`;
+            vector.innerHTML = `${-Number(parsedAngleFixed)}<span class="dereceIsareti">°</span>`;
+            vector3.innerHTML = `${180 + Number(parsedAngleFixed)}<span class="dereceIsareti">°</span>`;
         } else {
-            aci.innerHTML = `${360 - parsedAngle}<span class="dereceIsareti">°</span>`;
-            aci2.innerHTML = `${180 + parsedAngle}<span class="dereceIsareti">°</span>`;
+            aci.innerHTML = `${360 - Number(parsedAngleFixed)}<span class="dereceIsareti">°</span>`;
+            aci2.innerHTML = `${180 + Number(parsedAngleFixed)}<span class="dereceIsareti">°</span>`;
 
-            vector.innerHTML = `${360 - parsedAngle}<span class="dereceIsareti">°</span>`;
-            vector3.innerHTML = `${180 + parsedAngle}<span class="dereceIsareti">°</span>`;
+            vector.innerHTML = `${360 - Number(parsedAngleFixed)}<span class="dereceIsareti">°</span>`;
+            vector3.innerHTML = `${180 + Number(parsedAngleFixed)}<span class="dereceIsareti">°</span>`;
         }
 
 
+        turevHesapla(Number(kenarUzunlugu))
 
-        turevHesapla(parseInt(kenarUzunlugu))
+        let alan = (Number(kenarUzunlugu) * sin(180 + parsedAngle) * tabanValue) +
+            (Number(kenarUzunlugu) ** 2 * sin(180 + parsedAngle) * cos(180 + parsedAngle));
 
-        let alan = (parseInt(kenarUzunlugu) * sin(180 + parsedAngle) * tabanValue) +
-            (parseInt(kenarUzunlugu) ** 2 * sin(180 + parsedAngle) * cos(180 + parsedAngle));
+        let alanAlt = (Number(kenarUzunlugu) * sin(179.9 + parsedAngle) * tabanValue) +
+            (Number(kenarUzunlugu) ** 2 * sin(179.9 + parsedAngle) * cos(179.9 + parsedAngle));
 
-        console.log(parsedAngle)
-        anlikAlanValue.innerText = `${alan}`;
+        let alanUst = (Number(kenarUzunlugu) * sin(180.1 + parsedAngle) * tabanValue) +
+            (Number(kenarUzunlugu) ** 2 * sin(180.1 + parsedAngle) * cos(180.1 + parsedAngle));
 
-        if (Math.abs(alan - tureveGoreAlan) < 50) {
+        if (alanAlt <= alan && alanUst <= alan) {
             maximumAlanaUlasildi.style.display = 'block'
+            anlikAlanValue.innerText = `${tureveGoreAlan}`
         }
+
         else {
             maximumAlanaUlasildi.style.display = 'none'
+            anlikAlanValue.innerText = `${alan}`;
         }
-        anlikSuYuksekligi = `${parseInt(kenarUzunlugu) * sin(180 + parsedAngle)}`
+
+
+        // if (Math.abs(alan - tureveGoreAlan) < 50) {
+
+        // }
+
+        anlikSuYuksekligi = `${Number(kenarUzunlugu) * sin(180 + parsedAngle)}`
         suYuksekligiValue.innerText = anlikSuYuksekligi;
-        console.log(alan, Math.abs(alan - tureveGoreAlan), tureveGoreAlan)
     }
 }
 
-
 // Su Çizgisi Ayarları
 
-function suCizgisi(id) {
-    const vector = document.getElementById(id);
+function suCizgisi() {
     const container = vector.parentElement;
     let isDragging = false;
 
@@ -169,7 +177,6 @@ function suCizgisi(id) {
                 }
 
 
-                // console.log(yFixed)
             }
         }
 
@@ -190,20 +197,18 @@ document.getElementById('kenarlarButon').addEventListener('click', function (eve
     kenarUzunlugu = kenarlar.value;
     guncelUzunlukValue.innerText = kenarUzunlugu
 
-    if (parseInt(kenarUzunlugu) > 500) {
+    if (Number(kenarUzunlugu) > 500) {
         kenarUzunlugu = '500';
         alert("uzunluk en fazla 500 birim olabilir. ")
         guncelUzunlukValue.innerText = "500";
     }
 
-    if (parseInt(kenarUzunlugu) < 0) {
+    if (Number(kenarUzunlugu) < 0) {
         kenarUzunlugu = '0';
     }
     vector.style.width = kenarUzunlugu + 'px';
     vector3.style.width = kenarUzunlugu + 'px';
 
-    // vectorSu.style.width = `${kenarUzunlugu * 2 + 600}px`;
-    // vectorSu.style.left = `calc(50% - ${(kenarUzunlugu * 2 + 600) / 2}px)`;
 
     kenarlar.value = ""
 });
@@ -211,11 +216,9 @@ document.getElementById('kenarlarButon').addEventListener('click', function (eve
 
 tabanAyarlaButon.addEventListener('click', function (event) {
     event.preventDefault();
+
     maximumAlanaUlasildi.style.display = 'none'
     vector2yazi.innerText = `${taban.value}`
-    tabanValue = parseInt(`${taban.value}`);
-
-    console.log(tabanValue)
+    tabanValue = Number(`${taban.value}`);
+    taban.value = ""
 });
-
-// H = Hipotenüs x Sinüs
